2806c10c8c7b765575f21528e4e052d73e2004e6
Evgenii <eapashkov@gmail.com>
2022-10-23 12:15:20 +0300

    Added Unit tests  1

--- src/test/java/CollectionsTest/CustomLinkedListTest.java
+++ src/test/java/CollectionsTest/CustomLinkedListTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CustomLinkedListTest {
+
     private CustomLinkedList<Integer> testList;
 
     @BeforeEach
