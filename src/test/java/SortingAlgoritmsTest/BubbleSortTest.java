3acb060c03c030b6721be8f674c34ce723f8cc9b
Evgenii <eapashkov@gmail.com>
2022-10-24 21:45:17 +0300

    Added Unit tests

--- src/test/java/SortingAlgoritmsTest/BubbleSortTest.java
+++ src/test/java/SortingAlgoritmsTest/BubbleSortTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import java.util.Comparator;
 public class BubbleSortTest {
     private CustomLinkedList<String> testList;
