062108ab2ea47c09ccfb8927da0c142401a27b8f
Evgenii <eapashkov@gmail.com>
2022-10-25 13:29:12 +0300

    added tests

--- src/test/java/CollectionsTest/CustomLinkedListTest.java
+++ src/test/java/CollectionsTest/CustomLinkedListTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
 public class CustomLinkedListTest {
 
     private CustomLinkedList<Integer> testList;
